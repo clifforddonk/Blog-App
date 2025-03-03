0fcb36371c6e1c1050519f26b936237bb1c50956
clifforddonk <clifforddonk@gmail.com>
2025-03-03 16:53:10 +0000

    Create userModel

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "./AuthProvider";
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -8,14 +9,16 @@ const nunito = Nunito({
 });
 
 export const metadata = {
-  title: "Blog App",
-  description: "A Modern Blog App",
+  title: "My Blog",
+  description: "A mordern blog site",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${nunito.variable} antialiased`}>{children}</body>
+      <body className={`${nunito.variable} font-sans antialiased`}>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
