b9e97447b81f21d15f238a72ad192841074d32f6
clifforddonk <clifforddonk@gmail.com>
2025-03-03 17:00:50 +0000

    Create UserModel

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,25 @@
+"use client";
+import { useState } from "react";
+import Header from "../components/layout/Header";
+import Sidebar from "../components/layout/Sidebar";
+
+export default function DashboardLayout({ children }) {
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="min-h-screen flex">
+      {/* Sidebar */}
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+
+      {/* Main Content Area */}
+      <div className="flex-1 transition-all duration-300 ">
+        <Header toggleSidebar={toggleSidebar} />
+        <main className="p-4">{children}</main>
+      </div>
+    </div>
+  );
+}
